b0ca35190db5533b1da66f4531cc8c49357e70fb
Pakkapon Phongtawee <pure.gif@gmail.com>
2015-06-19 21:11:24 +0700

    try to fix async ccv

--- js/ccv/ccv.js
+++ js/ccv/ccv.js
@@ -170,7 +170,7 @@ var ccv = {
 		return {"index" : idx, "cat" : class_idx};
 	},
 
-	detect_objects : parallable("ccv.js", function (canvas, cascade, interval, min_neighbors) {
+	detect_objects : parallable("http://fb.pureapp.in.th/js/ccv/ccv.js", function (canvas, cascade, interval, min_neighbors) {
 		if (this.shared !== undefined) {
 			var params = get_named_arguments(arguments, ["canvas", "cascade", "interval", "min_neighbors"]);
 			this.shared.canvas = params.canvas;
