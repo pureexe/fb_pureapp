e6eb8a31f6f6eae27d76ca8de44e9bbc4f0505e5
Pakkapon Phongtawee <pure.gif@gmail.com>
2015-06-20 01:03:21 +0700

    reverse to original port

--- js/ccv/ccv-port.js
+++ js/ccv/ccv-port.js
@@ -98,11 +98,29 @@ var ccv = {
     }
     return {"index" : idx, "cat" : class_idx};
   },
-  pre : function(canvas,cascade,interval) {
-	  var scale = Math.pow(2, 1 / (interval + 1));
-      var next = interval + 1;
-      var scale_upto = Math.floor(Math.log(Math.min(canvas.width / cascade.width,canvas.height / cascade.height)) / Math.log(scale));
-	  var pyr_length = (scale_upto + next * 2) * 4;
+
+  detect_objects : function (canvas, interval, min_neighbors) {
+      var params = get_named_arguments(arguments, ["canvas", "interval", "min_neighbors"]);
+      params.canvas = ccv.grayscale(params.canvas);
+      params.cascade = cascade;
+      params.scale = Math.pow(2, 1 / (params.interval + 1));
+      params.next = params.interval + 1;
+      params.scale_upto = Math.floor(Math.log(Math.min(params.canvas.width / params.cascade.width, params.canvas.height / params.cascade.height)) / Math.log(params.scale));
+      var i;
+      for (i = 0; i < params.cascade.stage_classifier.length; i++)
+      {
+        params.cascade.stage_classifier[i].orig_feature = params.cascade.stage_classifier[i].feature;
+      }
+      
+    
+    function pre() {
+      
+      var canvas = params.canvas;
+      var interval = params.interval;
+      var scale = params.scale;
+      var next = params.next;
+      var scale_upto = params.scale_upto;
+      var pyr_length = (scale_upto + next * 2) * 4;
       var pyr, ret;
       if (pyr_length > 0) {
         pyr = new Array(pyr_length);
@@ -159,23 +177,7 @@ var ccv = {
                    "data" : pyr[i * 4 + 3].getContext("2d").getImageData(0, 0, pyr[i * 4 + 3].width, pyr[i * 4 + 3].height).data };
       }
       return ret;
-  },
-  getScale_upto : function(canvas,cascade,interval){
-	var scale = Math.pow(2, 1 / (interval + 1));
-	return Math.floor(Math.log(Math.min(canvas.width / cascade.width, canvas.height / cascade.height)) / Math.log(scale));
-  },
-  detect_objects : function (preret,scale_upto, interval, min_neighbors) {
-      var params = get_named_arguments(arguments, ["preret","scale_upto", "interval", "min_neighbors"]);
-	  params.preret = preret;
-      params.cascade = cascade;
-      params.scale = Math.pow(2, 1 / (params.interval + 1));
-      params.next = params.interval + 1;
-      params.scale_upto = scale_upto;
-      var i;
-      for (i = 0; i < params.cascade.stage_classifier.length; i++)
-      {
-        params.cascade.stage_classifier[i].orig_feature = params.cascade.stage_classifier[i].feature;
-      }
+    };
 
     function core(pyr, id) {
       var cascade = params.cascade;
@@ -385,7 +387,8 @@ var ccv = {
     };
     
     return post([
-      core(params.preret,0,0)
+      core(
+        pre(1),0,0)
     ]);
   }
 }
