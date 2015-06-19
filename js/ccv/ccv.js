57099afd1ba1fc45d239eb5c2d0f3edad20a5af9
Pakkapon Phongtawee <pure.gif@gmail.com>
2015-06-19 21:58:54 +0700

    test remove first line

--- js/ccv/ccv.js
+++ js/ccv/ccv.js
@@ -1,5 +1,6 @@
-var blob = new Blob(["onmessage = function (event) { var data = (typeof event.data == \"string\") ? JSON.parse(event.data) : event.data; var scope = { \"shared\" : data.shared };var result = parallable.core[data.name].apply(scope, [data.input, data.id, data.worker]);try {postMessage(result);} catch (e) {postMessage(JSON.stringify(result));}}"],{type : 'text/javascript'});
-var blobURL = window.URL.createObjectURL(blob);
+//var blob = new Blob(["onmessage = function (event) { var data = (typeof event.data == \"string\") ? JSON.parse(event.data) : event.data; var scope = { \"shared\" : data.shared };var result = parallable.core[data.name].apply(scope, [data.input, data.id, data.worker]);try {postMessage(result);} catch (e) {postMessage(JSON.stringify(result));}}"],{type : 'text/javascript'});
+//var blobURL = window.URL.createObjectURL(blob);
+var blobURL = "";
 if (parallable === undefined) {
 	var parallable = function (file, funct) {
 		parallable.core[funct.toString()] = funct().core;
